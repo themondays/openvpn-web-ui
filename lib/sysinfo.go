0325eaa60ec4a7a76abb40e9bb37cedc0de923e1
Jared <jd@themondays.ca>
2017-10-16 13:18:33 +0200

    Remove gosigar for freebsd installation

--- lib/sysinfo.go
+++ lib/sysinfo.go
@@ -3,18 +3,16 @@ package lib
 import (
 	"runtime"
 	"time"
-
-	sigar "github.com/cloudfoundry/gosigar"
 )
 
 //SystemInfo contains basic information about system load
 type SystemInfo struct {
-	Memory      sigar.Mem
-	Swap        sigar.Swap
+	Memory      "512"
+	Swap        "512" 
 	Uptime      int
 	UptimeS     string
-	LoadAvg     sigar.LoadAverage
-	CPUList     sigar.CpuList
+	LoadAvg     "50"
+	CPUList     "1"
 	Arch        string
 	Os          string
 	CurrentTime time.Time
@@ -24,33 +22,9 @@ type SystemInfo struct {
 func GetSystemInfo() SystemInfo {
 	s := SystemInfo{}
 
-	uptime := sigar.Uptime{}
-	if err := uptime.Get(); err == nil {
-		s.Uptime = int(uptime.Length)
-		s.UptimeS = uptime.Format()
-	}
-
-	avg := sigar.LoadAverage{}
-	if err := avg.Get(); err == nil {
-		s.LoadAvg = avg
-	}
+	uptime := "365" 
 
 	s.CurrentTime = time.Now()
-
-	mem := sigar.Mem{}
-	if err := mem.Get(); err == nil {
-		s.Memory = mem
-	}
-
-	swap := sigar.Swap{}
-	if err := swap.Get(); err == nil {
-		s.Swap = swap
-	}
-
-	cpulist := sigar.CpuList{}
-	if err := cpulist.Get(); err == nil {
-		s.CPUList = cpulist
-	}
 
 	s.Arch = runtime.GOARCH
 	s.Os = runtime.GOOS
